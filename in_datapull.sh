3faf9c1312999c60e75bf8a04815cc62845a9f4e
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2022-01-14 14:34:13 +0000

    increased requested time for the initiation script

--- in_datapull.sh
+++ in_datapull.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --time=01:00:00
+#SBATCH --time=15:00:00
 #SBATCH --job-name=boldpull
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=8G
-#SBATCH --partition=short
+#SBATCH --partition=medium
 #SBATCH --output=out.txt # the name of the output files
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=david.hemprich-bennett@zoo.ox.ac.uk
